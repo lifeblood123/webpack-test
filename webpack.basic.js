6ab896dc26af2f15c2f4274f3d63adafc7006b2e
lifeblood123 <zhangyuner112@163.com>
2019-04-20 22:51:05 +0800

    es6 test

---
+++ webpack.basic.js
@@ -0,0 +1,10 @@
+const path = require('path');
+console.log(__dirname);//d:/webpack-test  总工程的路径
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundletest.js',
+    path: path.resolve(__dirname, 'dist')//__dirname 是总工程路径
+  }
+};
