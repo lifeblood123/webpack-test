cd69dd2365c77a9f0719c000e8371cf9842c1f00
lifeblood123 <zhangyuner112@163.com>
2019-04-21 22:56:27 +0800

    两个入口

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 import yun from './image/yun.jpg';
-function component(){
-    var element = document.createElement('div');
-    // Lodash, currently included via a script, is required for this line to work
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['hello','webpack'],'');
-    element.classList.add('hello');
-     // 将图像添加到我们现有的 div。
-   var myIcon = new Image();
-   myIcon.src = yun;
-   element.appendChild(myIcon);
-   return element;
+import printMe from './print.js';
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['hello', 'webpack'], '');
+  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+  // 将图像添加到我们现有的 div。
+  //  var myIcon = new Image();
+  //  myIcon.src = yun;
+  //  element.appendChild(myIcon);
+  return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,15 @@ const path = require('path');
 console.log(__dirname);//d:/webpack-test  总工程的路径
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+
   output: {
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')//__dirname 是总工程路径
   },
   module: {
